972614844c2105ce15bb577d29d53b9a7a068323
Nathaniel Cole Tillery <nate.tillery@gmail.com>
2026-09-12 17:56:54 -0400

    Remake project into a cmake

---
+++ Weather-Dashboard/Weather-Dashboard.cpp
@@ -0,0 +1,12 @@
+﻿// Weather-Dashboard.cpp : Defines the entry point for the application.
+//
+
+#include "Weather-Dashboard.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
